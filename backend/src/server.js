3ef54127c4a97fd64730a15fd0ff71e08777d724
Vinicius Tartari <vinitartari@gmail.com>
2020-11-06 15:37:44 +0000

    Added morgan middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
+app.use(morgan('dev'));
 
 mongoose.connect('mongodb://localhost:27017/codechallenge', {
   useNewUrlParser: true,

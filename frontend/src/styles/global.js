f783d875e36bafda1321d5e0fc9f6dfe35d70e57
Vinicius Tartari <vinitartari@gmail.com>
2020-11-07 10:48:01 +0000

    Added global styles

--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -2,19 +2,18 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
   * {
-    margin:0;
-    padding:0;
-    outline:0;
-    box-sizing:border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
   }
 
   body {
-    background: #F0F0F5;
     --webkit-font-smoothing: antialised;
   }
 
   body, input, button {
-    font: 16px sans-serif;
+    font: 15px sans-serif;
   }
 
   button {

b5ec645b4dabe20aa7cafbe3e07d5b3a65e592fe
Vinicius Tartari <vinitartari@gmail.com>
2020-11-07 10:47:25 +0000

    Updated server config

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const morgan = require('morgan');
+
+const mongoose = require('mongoose');
+const mongoUrl = 'mongodb://localhost:27017/codechallenge';
 
 const app = express();
 
@@ -11,13 +13,26 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-mongoose.connect('mongodb://localhost:27017/codechallenge', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  mongoUrl,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  },
+  function (err) {
+    if (err) {
+      throw err;
+    } else {
+      console.log('MongoDB connected.');
+    }
+  },
+);
 
 requireDir('./models');
 
 app.use('/api', require('./routes'));
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Server running on port 3333');
+});
